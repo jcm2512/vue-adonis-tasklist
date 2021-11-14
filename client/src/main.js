7fa4a75c3358234143f7361069106c7103d93c36
jcm2512 <joe.magalotti@gmail.com>
2021-11-15 02:26:06 +0900

    init

--- client/src/main.js
+++ client/src/main.js
@@ -1,9 +1,15 @@
+import { sync } from 'vuex-router-sync';
+import Vuetify from 'vuetify';
+
 import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
 
 Vue.config.productionTip = false;
+
+sync(store, router);
+Vue.use(Vuetify);
 
 new Vue({
   router,
--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,9 +1,12 @@
+import createPersistedState from 'vuex-persistedstate';
+
 import Vue from 'vue';
 import Vuex from 'vuex';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
+  strict: true,
   state: {
   },
   mutations: {
@@ -12,4 +15,7 @@ export default new Vuex.Store({
   },
   modules: {
   },
+  plugins: [
+    createPersistedState(),
+  ],
 });
