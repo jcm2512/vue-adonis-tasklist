3183d80ffaae4ea23976de796e7df252b89c6fa9
jcm2512 <joe.magalotti@gmail.com>
2021-11-14 13:40:23 +0900

    create and index projects

--- server/app/Controllers/Http/ProjectController.js
+++ server/app/Controllers/Http/ProjectController.js
@@ -1,9 +1,22 @@
 'use strict'
 
+const Project = use('App/Models/Project')
+
 class ProjectController {
     async index({auth}) {
-        const user = await auth.getUser();
+        const user = await auth.getUser(); // grab user from lucid model
         return await user.projects().fetch()
+    }
+
+    async create({auth, request}){
+        const user = await auth.getUser(); // grab user from lucid model
+        const { title } = request.all() // grab title from payload
+        const project = new Project() // create project and fill in title   
+        project.fill({
+            title,
+        })
+        await user.projects().save(project) // associate project with user
+        return project
     }
 }
 
